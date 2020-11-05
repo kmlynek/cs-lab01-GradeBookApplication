e2889f1f6dc858bd5203f7cb66e5a3b5e36ee64b
Karol Mlynek <kmlynek65@gmail.com>
2020-11-05 21:43:01 +0100

    Task 6

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,5 +7,21 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (averageGrade >= 20)
+                return 'A';
+            else if (averageGrade >= 40)
+                return 'B';
+            else if (averageGrade >= 50)
+                return 'C';
+            else if (averageGrade >= 50)
+                return 'C';
+            else if (averageGrade >= 80)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
